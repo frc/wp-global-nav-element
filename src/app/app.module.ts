9a30976b9d7c6b7ea234973647f3042e8b5d2b2d
Samuli Ristimäki <samuli.ristimaki@frantic.com>
2018-05-28 17:31:36 +0300

    Init firebase config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,25 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+const config = { 
+  apiKey: "AIzaSyD4ACaSUieDKMJMRH3vTlJPkl4lD38qoj0",
+  authDomain: "wordpress-global-nav-67052.firebaseapp.com",
+  databaseURL: "https://wordpress-global-nav-67052.firebaseio.com",
+  projectId: "wordpress-global-nav-67052",
+  storageBucket: "wordpress-global-nav-67052.appspot.com",
+  messagingSenderId: "34893503013"
+};
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(config),
+    AngularFirestoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
